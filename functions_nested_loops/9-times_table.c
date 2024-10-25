f4b0ecd03522ae17d203786d5d0e6cceac73b843
Crystal-holberton <9188@holbertonstudents.com>
2024-10-25 05:24:34 +0000

    Times Table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,18 +15,20 @@ void times_table(void)
 		while (y <= 9)
 		{
 			z = x * y;
-			if (y > 0)
+			if (z >= 10)
 			{
-				_putchar(' ');
-			}
-			if (z < 10)
-			{
-				_putchar('0' + z);
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
 			}
 			else
 			{
-				_putchar('0' + z / 10);
-				_putchar('0' + z % 10);
+				_putchar(32);
+				_putchar(z + '0');
+			}
+			if (y < 9)
+			{
+				_putchar(44);
+				_putchar(32);
 			}
 			y++;
 		}
