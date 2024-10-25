c16beb2727b8ede2ed8838bb7babdd336d4ecef5
Crystal-holberton <9188@holbertonstudents.com>
2024-10-25 03:59:26 +0000

    Times Table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Returns: void
+ */
+
+void times_table(void)
+{
+	int x = 0, y;
+
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			int z = x * y;
+
+			if (z >= 9)
+			{
+				_putchar((z / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+
+			_putchar((z % 10) + '0');
+
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
