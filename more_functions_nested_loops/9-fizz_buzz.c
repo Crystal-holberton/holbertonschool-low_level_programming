6e1f974a6d681626314b41954e4ed70f7b951574
Crystal-holberton <9188@holbertonstudents.com>
2024-10-28 04:18:26 +0000

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int z = 1;
+
+	while (z <= 100)
+	{
+		if ((z % 3 == 0) && (z % 5 == 0))
+		{
+			printf("FizzBuzz");
+			z++;
+		}
+		else if (z % 3 == 0)
+		{
+			printf("Fizz");
+			z++;
+		}
+		else if (z % 5 == 0)
+		{
+			printf("Buzz");
+			z++;
+		}
+		else
+		{
+			printf("%d", z);
+			z++;
+		}
+		if (z != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
