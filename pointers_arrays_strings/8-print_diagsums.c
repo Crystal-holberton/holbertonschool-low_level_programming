3ebd62dc90eba3e7f7605a78b0b39478aa744bcc
Crystal-holberton <9188@holbertonstudents.com>
2024-11-06 03:00:12 +0000

    print diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,19 +12,16 @@
 void print_diagsums(int *a, int size)
 {
 	int row = 0;
-	int col = size - 1;
+	int col = 1;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	while (row <= (size * size))
+	while (row < size)
 	{
-		sum1 += a[row];
-		row = row + size + 1;
-	}
-	while (col <= (size * size - 1))
-	{
-		sum2 += a[col];
-		col = col + size - 1;
+		sum1 = sum1 + a[(size + 1) * row];
+		sum2 = sum2 + a[(size - 1) * col];
+		col++;
+		row++;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
