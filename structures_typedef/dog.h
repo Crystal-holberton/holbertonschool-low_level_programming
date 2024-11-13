5d80c780602cc71220c7c4a60563f4cb446dfc5d
Crystal-holberton <9188@holbertonstudents.com>
2024-11-13 00:24:20 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - listing dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
