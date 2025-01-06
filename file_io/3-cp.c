d23eb87d6f85535294b655a2f4210de22320754a
Crystal-holberton <9188@holbertonstudents.com>
2025-01-06 08:45:02 +0000

    cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,43 +6,43 @@
 #define BUFFER_SIZE 1024
 
 /**
- * print_error - prints error
- * @message: error format string
- * @filename: filename causing error
- * @exit_code: exit code
+ * print_error - Prints an error message
+ * @message: error message to print
+ * @filename: The name of the file
+ * @exit_code: The exit code
  * Return: void
  */
 void print_error(const char *message, const char *filename, int exit_code)
 {
 	if (filename)
 	{
-		dprintf(STDOUT_FILENO, "%s %s\n", message, filename);
+		dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 	}
 	else
 	{
-		dprintf(STDOUT_FILENO, "%s\n",  message);
+		dprintf(STDERR_FILENO, "%s\n", message);
 	}
 	exit(exit_code);
 }
 
 /**
- * close_file - closes file
- * @fd: The file descriptor to be closed
+ * close_file - Closes a file
+ * @fd: The file descriptor
  * Return: void
  */
 void close_file(int fd)
 {
 	if (close(fd) == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
 
 /**
- * copy_file - copies file
- * @file_from: original copied file
- * @file_to: new copied file
+ * copy_file - Copies the content of one file to another
+ * @file_from: The source file
+ * @file_to: The destination file
  * Return: void
  */
 void copy_file(const char *file_from, const char *file_to)
@@ -59,18 +59,24 @@ void copy_file(const char *file_from, const char *file_to)
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
+		close_file(fd_from);
 		print_error("Error: Can't write to", file_to, 99);
 	}
+
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
+			close_file(fd_from);
+			close_file(fd_to);
 			print_error("Error: Can't write to", file_to, 99);
 		}
 	}
 	if (bytes_read == -1)
 	{
+		close_file(fd_from);
+		close_file(fd_to);
 		print_error("Error: Can't read from file", file_from, 98);
 	}
 	close_file(fd_from);
@@ -78,16 +84,16 @@ void copy_file(const char *file_from, const char *file_to)
 }
 
 /**
- * main - copies content of file to another file
- * @argc: number of arguments
- * @argv: argument vector
- * Return: 0 on success, otherwise exit with error code
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: The argument vector
+ * Return: Always 0 on success
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		dprintf(STDOUT_FILENO, "97\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	copy_file(argv[1], argv[2]);
